51880f9eb747f01764295333f58223914be04ae0
Pascal Fenkam <62715296+open-sudo@users.noreply.github.com>
2023-10-17 07:46:42 -0400

    Update midterms.py

--- midterms.py
+++ midterms.py
@@ -2,58 +2,58 @@
 
 import sys
 
-def compute_stats(sourceFile: str, statsFile:str):
-  data =open(sourceFile,'r')
+def compute_stats(source_file: str, stats_file:str):
+  data =open(source_file,'r')
   lines=data.readlines()
 
-  gradeLines=lines[1:]
+  grade_lines=lines[1:]
   grades={}
-  for grade in gradeLines:
+  for grade in grade_lines:
       if len(grade.strip())> 0 :
          parts=grade.split(',')
          grades[parts[0].strip()]=int(parts[1].strip())
 
-  gradeValues=list(grades.values())
+  grade_values=list(grades.values())
 
-  gradeMin=100
-  for gradeValue in gradeValues:
-     if gradeValue < gradeMin :
-        gradeMin=gradeValue
+  grade_min=100
+  for grade_value in grade_values:
+     if grade_value < grade_min :
+        grade_min=grade_value
 
-  gradeMax=0
-  for gradeValue in gradeValues:
-     if gradeValue > gradeMax :
-        gradeMax=gradeValue
+  grade_max=0
+  for grade_value in grade_values:
+     if grade_value > grade_max :
+        grade_max=grade_value
 
   average=0
-  for gradeValue in gradeValues:
-        average=average+gradeValue
-  average = int(average / len(gradeValues))
+  for grade_value in grade_values:
+        average=average+grade_value
+  average = int(average / len(grade_values))
 
   tops=[]
   gradeIds=grades.keys()
-  for gradeId in gradeIds :
-     if grades[gradeId] == gradeMax :
-         tops.append(gradeId)
+  for grade_id in gradeIds :
+     if grades[grade_id] == grade_max :
+         tops.append(grade_id)
 
-  gradeValues.sort()
+  grade_values.sort()
   median=0;
-  numberOfStudents=len(gradeValues)
-  if numberOfStudents % 2 == 1 :
-     median=gradeValues[int(numberOfStudents/2)]
+  number_of_students=len(grade_values)
+  if number_of_students % 2 == 1 :
+     median=grade_values[int(number_of_students/2)]
   else:
-     median=(gradeValues[int(numberOfStudents/2)-1]+gradeValues[int(numberOfStudents/2)])/2
+     median=(gradeValues[int(number_of_students/2)-1]+grade_values[int(number_of_students/2)])/2
 
-  output=open(statsFile,'w')
-  output.write('Minimum Grade:'+str(gradeMin)+'\n')
-  output.write('Maximum Grade:'+str(gradeMax)+'\n')
+  output=open(stats_file,'w')
+  output.write('Minimum Grade:'+str(grade_min)+'\n')
+  output.write('Maximum Grade:'+str(grade_max)+'\n')
   output.write('Average Grade:'+str(average)+'\n')
   output.write('Top Students:'+(", ".join(tops))+'\n')
   output.write('Median Grade:'+str(median)+'\n')
   data.close()
   output.close()
 
-    
+
 def compute_median(grades: list) -> int:
   return 0
   
@@ -63,17 +63,17 @@ def compute_min(grades: list) -> int:
 def compute_max(grades: list) -> int:
   return 0
   
-def compute_top_students(grades: dict, gradeMax) -> list:
+def compute_top_students(grades: dict, grade_max) -> list:
   return []
          
 def compute_average(grades: list) -> float:
   return 0
   
-def read_grades(sourceFile: str) -> dict:
+def read_grades(source_file: str) -> dict:
   return {}
      
-def write_grades(min: int, max: int, med: int, avg: int, tops: list, statsFile:str):
+def write_grades(min: int, max: int, med: int, avg: int, tops: list, stats_file:str):
     return None
 
-if __name__ == '__main__' : 
+if __name__ == '__main__' :  
   compute_stats(sys.argv[1],sys.argv[2])
